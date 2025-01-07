2ec358a9373104bad8f1d4c343c3c7e8322b775b
CristianSomma <12999453@itisrossi.vi.it>
2025-01-07 22:22:23 +0100

    app terminata

--- scripts/home.js
+++ scripts/home.js
@@ -1,3 +1,9 @@
 function toMain() {
     window.location.href = 'docs/game.html';
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    if(window.innerWidth > 600){
+        toMain();
+    }
+})
